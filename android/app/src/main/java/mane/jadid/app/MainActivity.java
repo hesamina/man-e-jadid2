3c7e88dd5f36e6edf7e9478285000d2bcfa8d965
hesamina <81247612+hesamina@users.noreply.github.com>
2026-02-10 11:11:48 +0330

    Add files via upload

---
+++ android/app/src/main/java/mane/jadid/app/MainActivity.java
@@ -0,0 +1,5 @@
+package mane.jadid.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mane.jadid.app',
+  appName: 'ManeJadid',
+  webDir: 'dist'
+};
+
+export default config;
